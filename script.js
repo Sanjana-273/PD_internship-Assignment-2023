4416475e7fc849686c62c6ed8dce21c9641c1277
Sanjana-273 <sanjana.mahapatra@prodevans.com>
2023-09-20 14:46:36 +0530

    done

--- script.js
+++ script.js
@@ -1,38 +1,62 @@
-// Client 
-const token = 'BQBudYKQi7QKpWVy7uPhs5ZNSwd_1PO0jeOAu7LW8rZZI8gRP7HCmEzIWmo8lnf3K-prd8MUr2-pmWth0dQUq5YBE-y6JDW5eW0crZRgEhRtUvZ_J26X-MeayLyC3N3dx0Xmc2Hcn3RRdOwXQK7Xo6Yw7M9M_YrHLjk-TnALQ7_IHCyYhyHIB4QvNzvDHdDn9ePkt_4aGzMf0l6y-46tTSZEG8gukOThnK2mSXS5F52PUanqqFjH59QkmF1V2luh-qJMiftRSrkJM8fiRNJvaxyE';
-const url = "https://api.spotify.com/v1/artists?ids=06HL4z0CvFAxyc27GXpf02%2C0oOet2f43PA68X5RxKobEy%2C1dVygo6tRFXC8CSWURQJq2%2c6VuMaDnrHyPL1p4EHjYLi7%2c0C8ZW7ezQVs4URX5aX7Kqx%2c64KEffDW9EtZ1y2vBYgq8T%2c3eDT9fwXKuHWFvgZaaYC5v%2c4YRxDV8wJFPHPTeXepOstw%2c66CXWjxzNUsdJxJ2JdwvnR%2c3OLGltG8UPIea8sA4w0yg0"
 
-const request = new Request(
-    url,{
-        headers:{
-            'Authorization': `Bearer ${token}`
-        },
-})
+const token = 'BQDscYhXLSeV5KaGQT0b9p4Wg2F6c-b6dA46r407E9bV6DeWyso5_TPNUNEc3YQ8nvoajBDXcD-ZVBrW0_8lIU1zlW9d37E6voLP4xoTtHIfFCO_jw3HRVPFXd5IF8IHvjnd6X7tjG3lupayrVJmHaMJgl3-hS9FeHKj5LaWjsXdU-uI5ZDLdPqckc9aKffdDlDFGF_JIXHdiHjtBQ0';
+const artistIds = [
+  '06HL4z0CvFAxyc27GXpf02',
+  '0oOet2f43PA68X5RxKobEy',
+  '1dVygo6tRFXC8CSWURQJq2',
+  '6VuMaDnrHyPL1p4EHjYLi7',
+  '0C8ZW7ezQVs4URX5aX7Kqx',
+  '64KEffDW9EtZ1y2vBYgq8T',
+  '3eDT9fwXKuHWFvgZaaYC5v',
+  '4YRxDV8wJFPHPTeXepOstw',
+  '66CXWjxzNUsdJxJ2JdwvnR',
+  '3OLGltG8UPIea8sA4w0yg0'
+];
+
+const dataContainer = document.getElementById("data-container");
+
+async function fetchArtistData(artistId) {
+  const artistUrl = `https://api.spotify.com/v1/artists/${artistId}`;
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  };
+
+  try {
+    const response = await fetch(artistUrl, { headers });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching artist data:", error);
+    return null;
+  }
+}
 
 async function getData() {
-    try {
-        const response = await fetch(request);
-        const data = await response.json();
-        console.log(data);
-        
-        // Get the data-container element
-        const dataContainer = document.getElementById("data-container");
+  try {
+    const artistDataPromises = artistIds.map(fetchArtistData);
+    const artistDataArray = await Promise.all(artistDataPromises);
 
-        // Create an HTML structure to display the data (modify as needed)
-        const artistList = document.createElement("ul");
+    const artistList = document.createElement("ul");
 
-        // Loop through the data and create list items for each artist
-        data.artists.forEach(artist => {
-            const listItem = document.createElement("li");
-            listItem.textContent = `Artist Name: ${artist.name}, Popularity: ${artist.popularity}`;
-            artistList.appendChild(listItem);
-        });
+    artistDataArray.forEach(artist => {
+      if (artist) {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+            <img src="${artist.images[0].url}" alt="${artist.name}'s Image">
+          <h3>Artist Name: ${artist.name}</h3>
+          <p>Follower: ${artist.followers.total}</p>
+          <p>Music Kind: ${artist.genres.join(', ')}</p>
+          <p>Popularity: ${artist.popularity}</p>
+          
+        `;
+        artistList.appendChild(listItem);
+      }
+    });
 
-        // Append the artistList to the data-container
-        dataContainer.appendChild(artistList);
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
+    dataContainer.appendChild(artistList);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
-getData()
 
+getData();
